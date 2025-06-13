c2321452bd5aa756abdcf0020112ec7b163347f8
PepperShin <peep1996@naver.com>
2025-06-13 11:50:57 +0900

    commit

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'rest_framework',
+    'rest_framework', # dev_1
     'api',
 ]
 
@@ -56,7 +56,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"], # dev_1
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [

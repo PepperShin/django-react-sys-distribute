a0bd2b3a569f2ea707b11636b9c5c8baffb646b6
PepperShin <peep1996@naver.com>
2025-06-16 11:06:16 +0900

    commit

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-l5&#3$!3*kb21%crjusubu6a_-v7p+hrf!8308lp*=p_xigu8-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
